d1f6cd11f982b3b2e7efd9c35b0fa9715094e460
Anthony Guimard <bonjour@anthonyguimard.fr>
2024-10-20 02:33:53 +0200

    chore: remove releases config

--- config/releases.exs
+++
@@ -1,6 +0,0 @@
-import Config
-
-config :moodmate, MoodmateWeb.Endpoint,
-  url: [host: System.fetch_env!("HOST"), port: 443, scheme: "https"],
-  secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
-  auth_token_salt: System.fetch_env!("AUTH_TOKEN_SALT")
