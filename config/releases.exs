c8d1bc8187da80c4db34bf6166505b291d0e66e1
Anthony Guimard <bonjour@anthonyguimard.fr>
2024-10-20 02:25:47 +0200

    chore: adjust releases variables

--- config/releases.exs
+++ config/releases.exs
@@ -1,4 +1,6 @@
-# config/releases.exs
 import Config
 
-config :moodmate, MoodmateWeb.Endpoint, auth_token_salt: System.fetch_env!("AUTH_TOKEN_SALT")
+config :moodmate, MoodmateWeb.Endpoint,
+  url: [host: System.fetch_env!("HOST"), port: 443, scheme: "https"],
+  secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
+  auth_token_salt: System.fetch_env!("AUTH_TOKEN_SALT")
