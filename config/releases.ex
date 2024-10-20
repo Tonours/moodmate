89e3d0ea62127e3d839e08fce53642189d53908c
Anthony Guimard <bonjour@anthonyguimard.fr>
2024-10-20 02:51:51 +0200

    chore: relases config

---
+++ config/releases.ex
@@ -0,0 +1,6 @@
+import Config
+
+config :moodmate, MoodmateWeb.Endpoint,
+  url: [host: System.fetch_env!("PHX_HOST"), port: 443, scheme: "https"],
+  secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
+  auth_token_salt: System.fetch_env!("AUTH_TOKEN_SALT")
