8528970437f1c369f741b2ee393d7942d12e4d91
Anthony Guimard <bonjour@anthonyguimard.fr>
2024-10-20 03:03:39 +0200

    chore: debug host

--- config/releases.ex
+++ config/releases.ex
@@ -1,5 +1,6 @@
 import Config
 
 config :moodmate, MoodmateWeb.Endpoint,
+  url: [host: "localhost", port: 4000],
   secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
   auth_token_salt: System.fetch_env!("AUTH_TOKEN_SALT")
